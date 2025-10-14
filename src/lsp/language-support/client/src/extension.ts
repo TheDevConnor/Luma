fbcf06dec38865cf048db087835a5deb5c9b8809
John <92063969+TheSovietPancakes@users.noreply.github.com>
2025-10-13 21:55:57 -0500

    created/fixed vscode language extension (#2)

---
+++ src/lsp/language-support/client/src/extension.ts
@@ -0,0 +1,49 @@
+import { workspace, ExtensionContext, commands, window } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+  const serverOptions: ServerOptions = {
+    //? the name of the project is "luma" in the Makefile so this will be the name of exe for users
+    command: "luma",
+    transport: TransportKind.stdio,
+    args: ["-lsp"]
+  };
+
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for all documents by default
+    documentSelector: [{ scheme: "file", language: "lux" }],
+    synchronize: {
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "lux-lsp",
+    "Lux LSP",
+    serverOptions,
+    clientOptions
+  );
+
+  // Start the client. This will also launch the server
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
